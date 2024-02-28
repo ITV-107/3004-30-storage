4d2875b2468ac1da834faa37fc7bf24473066e12
ITV-107 <91462052+ITV-107@users.noreply.github.com>
2024-02-28 16:12:22 +0000

    Creating initial config for docker compose

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [[ $EUID -ne 0 ]]
+then
+  echo "Script requires root privileges to run"
+  exit
+fi
+
+echo "Successful import."
+
+# Install docker and docker compose.
+apt-get install -y docker.io docker-compose
+
+# Pull the base image for load balancer
+docker pull nginx:alpine
+
+# Pull the base image for backend web server.
+docker pull php:fpm
+
+# Run docker compose with the following scripts.
+docker-compose up -d
--- install_2.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [[ $EUID -ne 0 ]]
-then
-  echo "Script requires root privileges to run"
-  exit
-fi
-
-echo "Yay! Everything works!"
